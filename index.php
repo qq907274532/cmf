ac1dec37b1ccf1a105462e8add8768f7db0dafe1
qq907274532 <907274532@qq.com>
2016-12-08 18:06:06 +0800

    增加了logs函数

--- index.php
+++ index.php
@@ -16,10 +16,11 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
-
-// 定义应用目录
-define('APP_PATH','./App/');
 define('SITE_PATH',str_replace('\\','/',realpath(dirname(__FILE__).'/')));
+// 定义应用目录
+define('APP_PATH',SITE_PATH.'/App/');
+
+define('__APP_LOGS_PATH__' ,APP_PATH.'Logs');
 
 defined('RUNTIME_PATH') or define('RUNTIME_PATH',   SITE_PATH.'/Runtime/');   // 系统运行时目录
 // 引入ThinkPHP入口文件
